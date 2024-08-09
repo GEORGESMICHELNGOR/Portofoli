040ed5f7b159797deb8bbc632c42ab91ce116749
georgesndene <ndenegeorgesmichelngor@gmail.com>
2024-08-09 16:57:31 +0000

    first commit

---
+++ components/main/Projects.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import ProjectCard from "../sub/ProjectCard";
+
+const Projects = () => {
+  return (
+    <div
+      className="flex flex-col items-center justify-center py-20"
+      id="projects"
+    >
+      <h1 className="text-[40px] font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 py-20">
+        Mes Projets
+      </h1>
+      <div className="h-full w-full flex flex-col md:flex-row gap-10 px-10">
+        <ProjectCard
+          src="/nexte.jpeg"
+          title="Modern Next.js Portfolio"
+          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+        />
+        <ProjectCard
+          src="/gym.jpeg"
+          title="Interactive Website Cards"
+          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+        />
+        <ProjectCard
+          src="/hotel.jpeg"
+          title="Space Themed Website"
+          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Projects;
---
+++ constants/index.ts
@@ -0,0 +1,252 @@
+export const Skill_data = [
+    {
+      skill_name: "Html 5",
+      Image: "/html5.jpeg",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "Css",
+      Image: "/css.jpeg",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "Javascript",
+      Image: "/js.jpeg",
+      width: 65,
+      height: 65,
+    },
+    {
+      skill_name: "Tailwind Css",
+      Image: "/tailwindcss.jpeg",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "React",
+      Image: "/react.jpeg",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "Redux",
+      Image: "/redux.jpeg",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "React Query",
+      Image: "/react.jpeg",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "Type Script",
+      Image: "/typescript.jpeg",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "Next js 13",
+      Image: "/next.jpeg",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "Framer Motion",
+      Image: "/framer.jpeg",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "Stripe Payment",
+      Image: "/strype.jpeg",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "Node js",
+      Image: "/Node.jpeg",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "Mongo db",
+      Image: "/mongodb.png",
+      width: 40,
+      height: 40,
+    },
+  
+  ];
+  
+  export const Socials = [
+    {
+      name: "Discord",
+      src: "/Discord.svg",
+    },
+    {
+      name: "Facebook",
+      src: "/",
+    },
+    {
+      name: "Instagram",
+      src: "/discord.svg",
+    },
+  ];
+  
+  
+  
+  export const Frontend_skill = [
+    {
+      skill_name: "Html 5",
+      Image: "/html5.jpeg",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "Css",
+      Image: "/css.jpeg",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "Java Script",
+      Image: "/js.jpeg",
+      width: 65,
+      height: 65,
+    },
+    {
+      skill_name: "Tailwind Css",
+      Image: "/tailwindcss.jpeg",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "Material UI",
+      Image: "/ui.jpeg",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "React",
+      Image: "/react.jpeg",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "Redux",
+      Image: "/redux.jpeg",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "React Query",
+      Image: "/react.jpeg",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "Type Script",
+      Image: "/typescript.jpeg",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "Next js 13",
+      Image: "/next.jpeg",
+      width: 80,
+      height: 80,
+    },
+  ];
+  
+  export const Backend_skill = [
+    {
+      skill_name: "Node js",
+      Image: "/Node.jpeg",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "Express js",
+      Image: "/express.jpeg",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "Mongo db",
+      Image: "/mongo.jpeg",
+      width: 40,
+      height: 40,
+    },
+    {
+      skill_name: "git",
+      Image: "/git.jpeg",
+      width: 55,
+      height: 55,
+    },
+    {
+      skill_name: "github",
+      Image: "/github.jpeg",
+      width: 70,
+      height: 70,
+    },
+    {
+      skill_name: "My SQL",
+      Image: "/sql.jpeg",
+      width: 70,
+      height: 70,
+    },
+    {
+      skill_name: "Prisma",
+      Image: "/prisma.jpeg",
+      width: 70,
+      height: 70,
+    },
+    {
+      skill_name: "Graphql",
+      Image: "/graphql.png",
+      width: 80,
+      height: 80,
+    },
+  ];
+  
+  export const Full_stack = [
+    {
+      skill_name: "React Native",
+      Image: "/React native.jpeg",
+      width: 70,
+      height: 70,
+    },
+    
+    {
+      skill_name: "Docker",
+      Image: "/docker.jpeg",
+      width: 70,
+      height: 70,
+    },
+  
+    {
+      skill_name: "Figma",
+      Image: "/Figma.jpeg",
+      width: 50,
+      height: 50,
+    },
+  
+  ];
+  
+  
+  
+  export const Other_skill = [
+    {
+      skill_name: "Go",
+      Image: "/Go.jpeg",
+      width: 60,
+      height: 60,
+    },
+  ];
+  
+  
+  
+  
+  
