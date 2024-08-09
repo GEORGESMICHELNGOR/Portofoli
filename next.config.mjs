8a4e541381848527c57e2ba721b54e5acd935940
georgesndene <ndenegeorgesmichelngor@gmail.com>
2024-08-09 17:54:05 +0000

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+// Obtenir le répertoire du fichier courant
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config) => {
+    // Configurer les alias
+    config.resolve.alias['@'] = path.resolve(__dirname, 'src'); // ou le répertoire approprié
+    return config;
+  },
+};
 
 export default nextConfig;
